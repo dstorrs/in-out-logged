c0058d1cfdfff0d4656edcb13ea6a0ea34b4562e
dstorrs <dstorrs.public@gmail.com>
2022-04-13 16:37:51 -0400

    added missing build dep

--- info.rkt
+++ info.rkt
@@ -2,11 +2,11 @@
 
 (define collection "in-out-logged")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("sandbox-lib" "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/in-out-logged.scrbl" ())))
 (define pkg-desc "Run some code with log messages before and after")
 (define pkg-authors '("David K. Storrs"))
 
 (define test-omit-paths '("test.rkt"))
 
-(define version "0.2")
+(define version "0.3")
